83efafc66fb97dfc4f2b66e3863d77820a20f068
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-03-23 10:58:12 -0300

    first 3d model

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { Sky, OrbitControls, useGLTF } from '@react-three/drei'
+
+
+function Shoe() {
+
+  const { scene } = useGLTF("/shoe.gltf")
+  return (
+    <primitive object={scene} scale={1} />
+  )
+}
+
+function Box() {
+  return (
+    <mesh>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color={"red"} />
+    </mesh>
+  )
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Canvas style={{ height: window.innerHeight }}>
+      <OrbitControls />
+      <Sky />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      {/* <Box /> */}
+      <Shoe />
+    </Canvas>
   );
 }
 
